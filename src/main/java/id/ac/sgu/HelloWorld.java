14115560603799749ecd22c7948955d5d2efeacc
Christoph Hümbert <christoph.huembert@commsult.de>
2020-10-18 17:03:44 +0200

    logging config

--- src/main/java/id/ac/sgu/HelloWorld.java
+++ src/main/java/id/ac/sgu/HelloWorld.java
@@ -1,9 +1,20 @@
 package id.ac.sgu;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+	private static final Logger LOG = LogManager.getLogger(HelloWorld.class);
 
+	public static void main(String[] args) {
+
+		// System.out.println("Hello World");
+		
+		LOG.trace("TRACE");
+		LOG.debug("DEBUG");
+		LOG.info("INFO");
+		LOG.warn("WARN");
+		LOG.error("ERROR");
+	}
 }
